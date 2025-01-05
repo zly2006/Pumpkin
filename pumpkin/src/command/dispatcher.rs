zly2006/Pumpkin

commit 2d136b1c246453bfea0a2cd209b11e6592b12407

fix: command permission message (#428)
* fix: command permission handling

Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

* fix: formatting

Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

---------

Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

pumpkin/src/command/dispatcher.rs
@@ -1,10 +1,11 @@
+use pumpkin_core::permission::PermissionLvl;
 use pumpkin_core::text::TextComponent;
 use pumpkin_protocol::client::play::CommandSuggestion;
 
 use super::args::ConsumedArgs;
 
 use crate::command::dispatcher::CommandError::{
-    GeneralCommandIssue, InvalidConsumption, InvalidRequirement, OtherPumpkin,
+    GeneralCommandIssue, InvalidConsumption, InvalidRequirement, OtherPumpkin, PermissionDenied,
 };
 use crate::command::tree::{Command, CommandTree, NodeType, RawArgs};
 use crate::command::CommandSender;
@@ -23,6 +24,8 @@ pub(crate) enum CommandError {
     /// Return this if a condition that a [`Node::Require`] should ensure is met is not met.
     InvalidRequirement,
 
+    PermissionDenied,
+
     OtherPumpkin(Box<dyn PumpkinError>),
 
     GeneralCommandIssue(String),
@@ -39,6 +42,10 @@ impl CommandError {
                 log::error!("Error while parsing command \"{cmd}\": a requirement that was expected was not met.");
                 Ok("Internal Error (See logs for details)".into())
             }
+            PermissionDenied => {
+                log::warn!("Permission denied for command \"{cmd}\"");
+                Ok("I'm sorry, but you do not have permission to perform this command. Please contact the server administrator if you believe this is an error.".into())
+            }
             GeneralCommandIssue(s) => Ok(s),
             OtherPumpkin(e) => Err(e),
         }
@@ -48,6 +55,7 @@ impl CommandError {
 #[derive(Default)]
 pub struct CommandDispatcher {
     pub(crate) commands: HashMap<String, Command>,
+    pub(crate) permissions: HashMap<String, PermissionLvl>,
 }
 
 /// Stores registered [`CommandTree`]s and dispatches commands to them.
@@ -113,6 +121,10 @@ impl CommandDispatcher {
                     log::error!("Error while parsing command \"{cmd}\": a requirement that was expected was not met.");
                     return Vec::new();
                 }
+                Err(PermissionDenied) => {
+                    log::warn!("Permission denied for command \"{cmd}\"");
+                    return Vec::new();
+                }
                 Err(GeneralCommandIssue(issue)) => {
                     log::error!("Error while parsing command \"{cmd}\": {issue}");
                     return Vec::new();
@@ -147,6 +159,14 @@ impl CommandDispatcher {
             .ok_or(GeneralCommandIssue("Empty Command".to_string()))?;
         let raw_args: Vec<&str> = parts.rev().collect();
 
+        let Some(permission) = self.permissions.get(key) else {
+            return Err(GeneralCommandIssue("Command not found".to_string()));
+        };
+
+        if !src.has_permission_lvl(*permission) {
+            return Err(PermissionDenied);
+        }
+
         let tree = self.get_tree(key)?;
 
         // try paths until fitting path is found
@@ -178,6 +198,10 @@ impl CommandDispatcher {
                 Ok(tree)
             }
         }
+    }
+
+    pub(crate) fn get_permission_lvl(&self, key: &str) -> Option<PermissionLvl> {
+        self.permissions.get(key).copied()
     }
 
     async fn try_is_fitting_path<'a>(
@@ -270,7 +294,7 @@ impl CommandDispatcher {
     }
 
     /// Register a command with the dispatcher.
-    pub(crate) fn register(&mut self, tree: CommandTree) {
+    pub(crate) fn register(&mut self, tree: CommandTree, permission: PermissionLvl) {
         let mut names = tree.names.iter();
 
         let primary_name = names.next().expect("at least one name must be provided");
@@ -280,6 +304,8 @@ impl CommandDispatcher {
                 .insert(name.to_string(), Command::Alias(primary_name.to_string()));
         }
 
+        self.permissions
+            .insert(primary_name.to_string(), permission);
         self.commands
             .insert(primary_name.to_string(), Command::Tree(tree));
     }
@@ -288,11 +314,11 @@ impl CommandDispatcher {
 #[cfg(test)]
 mod test {
     use crate::command::{default_dispatcher, tree::CommandTree};
-
+    use pumpkin_core::permission::PermissionLvl;
     #[test]
     fn test_dynamic_command() {
         let mut dispatcher = default_dispatcher();
         let tree = CommandTree::new(["test"], "test_desc");
-        dispatcher.register(tree);
+        dispatcher.register(tree, PermissionLvl::Zero);
     }
 }

pumpkin/src/command/client_cmd_suggestions.rs
@@ -20,6 +20,14 @@ pub async fn send_c_commands_packet(player: &Arc<Player>, dispatcher: &RwLock<Co
             continue;
         };
 
+        let Some(permission) = dispatcher.get_permission_lvl(key) else {
+            continue;
+        };
+
+        if !cmd_src.has_permission_lvl(permission) {
+            continue;
+        }
+
         let (is_executable, child_nodes) =
             nodes_to_proto_node_builders(&cmd_src, &tree.nodes, &tree.children);
 

pumpkin/src/command/commands/cmd_fill.rs
@@ -2,13 +2,12 @@ use crate::command::args::arg_block::BlockArgumentConsumer;
 use crate::command::args::arg_position_block::BlockPosArgumentConsumer;
 use crate::command::args::{ConsumedArgs, FindArg};
 use crate::command::tree::CommandTree;
-use crate::command::tree_builder::{argument, literal, require};
+use crate::command::tree_builder::{argument, literal};
 use crate::command::{CommandError, CommandExecutor, CommandSender};
 
 use async_trait::async_trait;
 use pumpkin_core::math::position::WorldPosition;
 use pumpkin_core::math::vector3::Vector3;
-use pumpkin_core::permission::PermissionLvl;
 use pumpkin_core::text::TextComponent;
 
 const NAMES: [&str; 1] = ["fill"];
@@ -157,19 +156,16 @@ impl CommandExecutor for SetblockExecutor {
 
 pub fn init_command_tree() -> CommandTree {
     CommandTree::new(NAMES, DESCRIPTION).with_child(
-        require(|sender| sender.has_permission_lvl(PermissionLvl::Two) && sender.world().is_some())
-            .with_child(
-                argument(ARG_FROM, BlockPosArgumentConsumer).with_child(
-                    argument(ARG_TO, BlockPosArgumentConsumer).with_child(
-                        argument(ARG_BLOCK, BlockArgumentConsumer)
-                            .with_child(literal("destroy").execute(SetblockExecutor(Mode::Destroy)))
-                            .with_child(literal("hollow").execute(SetblockExecutor(Mode::Hollow)))
-                            .with_child(literal("keep").execute(SetblockExecutor(Mode::Keep)))
-                            .with_child(literal("outline").execute(SetblockExecutor(Mode::Outline)))
-                            .with_child(literal("replace").execute(SetblockExecutor(Mode::Replace)))
-                            .execute(SetblockExecutor(Mode::Replace)),
-                    ),
-                ),
+        argument(ARG_FROM, BlockPosArgumentConsumer).with_child(
+            argument(ARG_TO, BlockPosArgumentConsumer).with_child(
+                argument(ARG_BLOCK, BlockArgumentConsumer)
+                    .with_child(literal("destroy").execute(SetblockExecutor(Mode::Destroy)))
+                    .with_child(literal("hollow").execute(SetblockExecutor(Mode::Hollow)))
+                    .with_child(literal("keep").execute(SetblockExecutor(Mode::Keep)))
+                    .with_child(literal("outline").execute(SetblockExecutor(Mode::Outline)))
+                    .with_child(literal("replace").execute(SetblockExecutor(Mode::Replace)))
+                    .execute(SetblockExecutor(Mode::Replace)),
             ),
+        ),
     )
 }

pumpkin/src/command/commands/cmd_gamemode.rs
@@ -4,7 +4,6 @@ use crate::command::args::arg_gamemode::GamemodeArgumentConsumer;
 use crate::command::args::GetCloned;
 
 use crate::TextComponent;
-use pumpkin_core::permission::PermissionLvl;
 
 use crate::command::args::arg_players::PlayersArgumentConsumer;
 
@@ -109,12 +108,10 @@ impl CommandExecutor for GamemodeTargetPlayer {
 #[allow(clippy::redundant_closure_for_method_calls)]
 pub fn init_command_tree() -> CommandTree {
     CommandTree::new(NAMES, DESCRIPTION).with_child(
-        require(|sender| sender.has_permission_lvl(PermissionLvl::Two)).with_child(
-            argument(ARG_GAMEMODE, GamemodeArgumentConsumer)
-                .with_child(require(|sender| sender.is_player()).execute(GamemodeTargetSelf))
-                .with_child(
-                    argument(ARG_TARGET, PlayersArgumentConsumer).execute(GamemodeTargetPlayer),
-                ),
-        ),
+        argument(ARG_GAMEMODE, GamemodeArgumentConsumer)
+            .with_child(require(|sender| sender.is_player()).execute(GamemodeTargetSelf))
+            .with_child(
+                argument(ARG_TARGET, PlayersArgumentConsumer).execute(GamemodeTargetPlayer),
+            ),
     )
 }

pumpkin/src/command/commands/cmd_give.rs
@@ -7,9 +7,8 @@ use crate::command::args::arg_item::ItemArgumentConsumer;
 use crate::command::args::arg_players::PlayersArgumentConsumer;
 use crate::command::args::{ConsumedArgs, FindArg, FindArgDefaultName};
 use crate::command::tree::CommandTree;
-use crate::command::tree_builder::{argument, argument_default_name, require};
+use crate::command::tree_builder::{argument, argument_default_name};
 use crate::command::{CommandError, CommandExecutor, CommandSender};
-use pumpkin_core::permission::PermissionLvl;
 
 const NAMES: [&str; 1] = ["give"];
 
@@ -76,12 +75,10 @@ impl CommandExecutor for GiveExecutor {
 
 pub fn init_command_tree() -> CommandTree {
     CommandTree::new(NAMES, DESCRIPTION).with_child(
-        require(|sender| sender.has_permission_lvl(PermissionLvl::Two)).with_child(
-            argument_default_name(PlayersArgumentConsumer).with_child(
-                argument(ARG_ITEM, ItemArgumentConsumer)
-                    .execute(GiveExecutor)
-                    .with_child(argument_default_name(item_count_consumer()).execute(GiveExecutor)),
-            ),
+        argument_default_name(PlayersArgumentConsumer).with_child(
+            argument(ARG_ITEM, ItemArgumentConsumer)
+                .execute(GiveExecutor)
+                .with_child(argument_default_name(item_count_consumer()).execute(GiveExecutor)),
         ),
     )
 }

pumpkin/src/command/commands/cmd_op.rs
@@ -2,14 +2,13 @@ use crate::{
     command::{
         args::{arg_players::PlayersArgumentConsumer, Arg, ConsumedArgs},
         tree::CommandTree,
-        tree_builder::{argument, require},
+        tree_builder::argument,
         CommandError, CommandExecutor, CommandSender,
     },
     data::{op_data::OPERATOR_CONFIG, SaveJSONConfiguration},
 };
 use async_trait::async_trait;
 use pumpkin_config::{op::Op, BASIC_CONFIG};
-use pumpkin_core::permission::PermissionLvl;
 use pumpkin_core::text::TextComponent;
 use CommandError::InvalidConsumption;
 
@@ -73,8 +72,6 @@ impl CommandExecutor for OpExecutor {
 }
 
 pub fn init_command_tree() -> CommandTree {
-    CommandTree::new(NAMES, DESCRIPTION).with_child(
-        require(|sender| sender.has_permission_lvl(PermissionLvl::Three))
-            .with_child(argument(ARG_TARGET, PlayersArgumentConsumer).execute(OpExecutor)),
-    )
+    CommandTree::new(NAMES, DESCRIPTION)
+        .with_child(argument(ARG_TARGET, PlayersArgumentConsumer).execute(OpExecutor))
 }

pumpkin/src/command/commands/cmd_say.rs
@@ -5,10 +5,9 @@ use pumpkin_protocol::client::play::CSystemChatMessage;
 use crate::command::{
     args::{arg_message::MsgArgConsumer, Arg, ConsumedArgs},
     tree::CommandTree,
-    tree_builder::{argument, require},
+    tree_builder::argument,
     CommandError, CommandExecutor, CommandSender,
 };
-use pumpkin_core::permission::PermissionLvl;
 use CommandError::InvalidConsumption;
 
 const NAMES: [&str; 1] = ["say"];
@@ -42,8 +41,6 @@ impl CommandExecutor for SayExecutor {
 }
 
 pub fn init_command_tree() -> CommandTree {
-    CommandTree::new(NAMES, DESCRIPTION).with_child(
-        require(|sender| sender.has_permission_lvl(PermissionLvl::Two))
-            .with_child(argument(ARG_MESSAGE, MsgArgConsumer).execute(SayExecutor)),
-    )
+    CommandTree::new(NAMES, DESCRIPTION)
+        .with_child(argument(ARG_MESSAGE, MsgArgConsumer).execute(SayExecutor))
 }

pumpkin/src/command/commands/cmd_seed.rs
@@ -1,9 +1,7 @@
-use crate::command::tree_builder::require;
 use crate::command::{
     args::ConsumedArgs, tree::CommandTree, CommandError, CommandExecutor, CommandSender,
 };
 use async_trait::async_trait;
-use pumpkin_core::permission::PermissionLvl;
 use pumpkin_core::text::click::ClickEvent;
 use pumpkin_core::text::hover::HoverEvent;
 use pumpkin_core::text::{color::NamedColor, TextComponent};
@@ -55,9 +53,5 @@ impl CommandExecutor for PumpkinExecutor {
 }
 
 pub fn init_command_tree() -> CommandTree {
-    CommandTree::new(NAMES, DESCRIPTION)
-        .with_child(require(|sender| {
-            sender.has_permission_lvl(PermissionLvl::Two)
-        }))
-        .execute(PumpkinExecutor)
+    CommandTree::new(NAMES, DESCRIPTION).execute(PumpkinExecutor)
 }

commit 3bb30c4413739d9dc4c46d91235a6f4e26b27288

fix: Alias command names
was broken in https://github.com/Pumpkin-MC/Pumpkin/commit/2d136b1c246453bfea0a2cd209b11e6592b12407#diff-f79fb5d460a0d374c892732876dd40efaced4a118291a777519d3965fe904195

pumpkin/src/command/dispatcher.rs
@@ -160,7 +160,9 @@ impl CommandDispatcher {
         let raw_args: Vec<&str> = parts.rev().collect();
 
         let Some(permission) = self.permissions.get(key) else {
-            return Err(GeneralCommandIssue("Command not found".to_string()));
+            return Err(GeneralCommandIssue(
+                "Permission for Command not found".to_string(),
+            ));
         };
 
         if !src.has_permission_lvl(*permission) {
@@ -302,6 +304,7 @@ impl CommandDispatcher {
         for name in names {
             self.commands
                 .insert(name.to_string(), Command::Alias(primary_name.to_string()));
+            self.permissions.insert(name.to_string(), permission);
         }
 
         self.permissions