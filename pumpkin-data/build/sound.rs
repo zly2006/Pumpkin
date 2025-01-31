e1e0576f29ce374c2bda6d44ad403d590391baf7
drakeerv <60192521+drakeerv@users.noreply.github.com>
2025-01-31 04:12:07 -0500

    Make sound to_name a const fn (#519)
    Small change but should make it a bit faster

--- pumpkin-data/build/sound.rs
+++ pumpkin-data/build/sound.rs
@@ -50,7 +50,7 @@ pub(crate) fn build() -> TokenStream {
                 }
             }
 
-            pub fn to_name(&self) -> &'static str {
+            pub const fn to_name(&self) -> &'static str {
                 match self {
                     #type_to_name
                 }
