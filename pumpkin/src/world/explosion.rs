2bc19bacb5d208e29296254603429a6b919cb881
Florian Lang <144521337+FlorianLang06@users.noreply.github.com>
2025-03-12 08:15:04 +0100

    Fix order of explosion block handling that droping and explotion of blocks work (#620)
    * fix: explosion set block_state to 0 after drop and explode
    
    * fix: load block before setting block_state to handle drop and explode of block after

--- pumpkin/src/world/explosion.rs
+++ pumpkin/src/world/explosion.rs
@@ -77,10 +77,12 @@ impl Explosion {
             if block_state.air {
                 continue;
             }
-            world.set_block_state(&pos, 0).await;
 
             let block = world.get_block(&pos).await.unwrap();
             let pumpkin_block = server.block_registry.get_pumpkin_block(&block);
+
+            world.set_block_state(&pos, 0).await;
+
             if pumpkin_block.is_none_or(|s| s.should_drop_items_on_explosion()) {
                 drop_loot(world, &block, &pos, false, block_state.id).await;
             }
