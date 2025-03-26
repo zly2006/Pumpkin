6563957685f80936c84de94bcc761180f090f04e
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2025-03-26 18:00:14 +0100

    fix: view distance goes not above 16 (#654)
    * fix: view distance goes no above 16
    
    * fix: cargo fmt
    
    * removed the floating point conversation
    
    * fixed spelling
    
    * renamed the test for the capacity estimation
    
    * edit: replaced division with bitshifting
    
    * added more ()
    
    * extendet test and fixed missing chunks when the view distance is very low
    
    * added commentar
    
    ---------
    
    Co-authored-by: unschlagbar <adrian@kuhlmann@gmx.de>

--- pumpkin-world/src/cylindrical_chunk_iterator.rs
+++ pumpkin-world/src/cylindrical_chunk_iterator.rs
@@ -54,30 +54,36 @@ impl Cylindrical {
     }
 
     pub fn is_within_distance(&self, x: i32, z: i32) -> bool {
-        let rel_x = ((x - self.center.x).abs() - 1).max(0);
-        let rel_z = ((z - self.center.z).abs() - 1).max(0);
+        let rel_x = ((x - self.center.x).abs() as i64 - 2).max(0);
+        let rel_z = ((z - self.center.z).abs() as i64 - 2).max(0);
 
-        let max_leg = (rel_x.max(rel_z) - 1).max(0) as i64;
-        let min_leg = rel_x.min(rel_z) as i64;
-
-        let hyp_sqr = max_leg * max_leg + min_leg * min_leg;
-        hyp_sqr < (self.view_distance.saturating_mul(self.view_distance).get() as i64)
+        let hyp_sqr = rel_x * rel_x + rel_z * rel_z;
+        //The view distance should be converted to i64 first because u8 * u8 can overflow
+        hyp_sqr < (self.view_distance.get() as i64).pow(2)
     }
 
     /// Returns an iterator of all chunks within this cylinder
     pub fn all_chunks_within(&self) -> Vec<Vector2<i32>> {
-        // This is a naive implementation: start with square and cut out ones that dont fit
-        let mut all_chunks = Vec::new();
+        // I came up with this values by testing
+        // for view distances 2-32 it usually gives 5 - 20 chunks more than needed if the player is on ground
+        // this looks scary but this few calculations are definitely faster than ~5 reallocations
+        // this part "3167) >> 10" is a replacement for flointing point multiplication
+        let estimated_capacity = ((self.view_distance.get() as usize + 3).pow(2) * 3167) >> 10;
+        let mut all_chunks = Vec::with_capacity(estimated_capacity);
+
         for x in self.left()..=self.right() {
-            for z in self.bottom()..=self.top() {
-                all_chunks.push(Vector2::new(x, z));
+            let mut was_in = false;
+            'inner: for z in self.bottom()..=self.top() {
+                if self.is_within_distance(x, z) {
+                    all_chunks.push(Vector2::new(x, z));
+                    was_in = true;
+                } else if was_in {
+                    break 'inner;
+                }
             }
         }
 
         all_chunks
-            .into_iter()
-            .filter(|chunk| self.is_within_distance(chunk.x, chunk.z))
-            .collect()
     }
 }
 
@@ -91,18 +97,40 @@ mod test {
 
     #[test]
     fn test_bounds() {
-        let cylinder = Cylindrical::new(Vector2::new(0, 0), unsafe { NonZeroU8::new_unchecked(1) });
-        for chunk in cylinder.all_chunks_within() {
-            assert!(chunk.x >= cylinder.left() && chunk.x <= cylinder.right());
-            assert!(chunk.z >= cylinder.bottom() && chunk.z <= cylinder.top());
-        }
+        let mut cylinder =
+            Cylindrical::new(Vector2::new(0, 0), unsafe { NonZeroU8::new_unchecked(1) });
 
-        for x in (cylinder.left() - 2)..=(cylinder.right() + 2) {
-            for z in (cylinder.bottom() - 2)..=(cylinder.top() + 2) {
-                if cylinder.is_within_distance(x, z) {
-                    assert!(x >= cylinder.left() && x <= cylinder.right());
-                    assert!(z >= cylinder.bottom() && z <= cylinder.top());
+        for view_distance in 1..=32 {
+            cylinder.view_distance = unsafe { NonZeroU8::new_unchecked(view_distance) };
+
+            for chunk in cylinder.all_chunks_within() {
+                assert!(chunk.x >= cylinder.left() && chunk.x <= cylinder.right());
+                assert!(chunk.z >= cylinder.bottom() && chunk.z <= cylinder.top());
+            }
+
+            for x in (cylinder.left() - 2)..=(cylinder.right() + 2) {
+                for z in (cylinder.bottom() - 2)..=(cylinder.top() + 2) {
+                    if cylinder.is_within_distance(x, z) {
+                        assert!(x >= cylinder.left() && x <= cylinder.right());
+                        assert!(z >= cylinder.bottom() && z <= cylinder.top());
+                    }
                 }
+            }
+        }
+    }
+
+    #[test]
+    fn all_chunks_within_capacity_estimation() {
+        let mut cylinder =
+            Cylindrical::new(Vector2::new(0, 0), unsafe { NonZeroU8::new_unchecked(1) });
+
+        for distance in 1..=64 {
+            cylinder.view_distance = unsafe { NonZeroU8::new_unchecked(distance) };
+            let chunks = cylinder.all_chunks_within();
+            let estimated_capacity = ((distance as usize + 3).pow(2) * 3167) >> 10;
+
+            if estimated_capacity < chunks.len() {
+                panic!()
             }
         }
     }
