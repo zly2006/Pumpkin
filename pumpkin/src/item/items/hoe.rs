2742086e6953d16431942be037291bb3af9f6325
ht06 <hthug06@gmail.com>
2025-04-03 19:02:11 +0200

    Make coarse and rooted dirt work as intended with the hoe (#688)
    * fix: redesign of hoe logic
    
    * refactor + tests passed
    
    * delete .clone + return the location on the match face

--- pumpkin/src/item/items/hoe.rs
+++ pumpkin/src/item/items/hoe.rs
@@ -1,13 +1,17 @@
+use crate::entity::item::ItemEntity;
 use crate::entity::player::Player;
 use crate::item::pumpkin_item::{ItemMetadata, PumpkinItem};
 use crate::server::Server;
 use crate::world::BlockFlags;
 use async_trait::async_trait;
 use pumpkin_data::block::Block;
+use pumpkin_data::entity::EntityType;
 use pumpkin_data::item::Item;
 use pumpkin_data::tag::Tagable;
 use pumpkin_util::math::position::BlockPos;
 use pumpkin_world::block::BlockDirection;
+use std::sync::Arc;
+
 pub struct HoeItem;
 
 impl ItemMetadata for HoeItem {
@@ -43,19 +47,54 @@ impl PumpkinItem for HoeItem {
             || block == &Block::COARSE_DIRT
             || block == &Block::ROOTED_DIRT
         {
+            let mut future_block = block;
             let world = player.world().await;
-            if face != &BlockDirection::Down
-                && world.get_block_state(&location.up()).await.unwrap().air
-            {
-                world
-                    .set_block_state(
-                        &location,
-                        Block::FARMLAND.default_state_id,
-                        BlockFlags::NOTIFY_ALL,
-                    )
-                    .await;
+
+            //Only rooted can be right-clicked on the bottom of the block
+            if face == &BlockDirection::Down {
+                if block == &Block::ROOTED_DIRT {
+                    future_block = &Block::DIRT;
+                }
+            } else {
+                // grass, dirt && dirt path become farmland
+                if (block == &Block::GRASS_BLOCK
+                    || block == &Block::DIRT_PATH
+                    || block == &Block::DIRT)
+                    && world.get_block_state(&location.up()).await.unwrap().air
+                {
+                    future_block = &Block::FARMLAND;
+                }
+                //Coarse dirt and rooted dirt become dirt
+                else if block == &Block::COARSE_DIRT || block == &Block::ROOTED_DIRT {
+                    future_block = &Block::DIRT;
+                }
+            }
+
+            world
+                .set_block_state(
+                    &location,
+                    future_block.default_state_id,
+                    BlockFlags::NOTIFY_ALL,
+                )
+                .await;
+
+            //Also rooted_dirt drop a hanging_root
+            if block == &Block::ROOTED_DIRT {
+                let location = match face {
+                    BlockDirection::Up => location.up().to_f64(),
+                    BlockDirection::Down => location.down().to_f64(),
+                    BlockDirection::North => location.up().to_f64().add_raw(0.0, -0.4, -1.0),
+                    BlockDirection::South => location.up().to_f64().add_raw(0.0, -0.4, 1.0),
+                    BlockDirection::West => location.up().to_f64().add_raw(-1.0, -0.4, 0.0),
+                    BlockDirection::East => location.up().to_f64().add_raw(1.0, -0.4, 0.0),
+                };
+                let entity = world.create_entity(location, EntityType::ITEM);
+                // TODO: Merge stacks together
+                let item_entity =
+                    Arc::new(ItemEntity::new(entity, Block::HANGING_ROOTS.item_id, 1).await);
+                world.spawn_entity(item_entity.clone()).await;
+                item_entity.send_meta_packet().await;
             }
         }
-        // TODO: implement hanging_roots
     }
 }
